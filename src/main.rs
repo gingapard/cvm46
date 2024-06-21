2de50860b0f1fb7482ec55926979c1b8ed793e79
gapard <cbroo@duck.com>
2024-06-21 18:23:20 +0200

    added basic instructions for vm

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 type Word = i64;
 const STACK_CAP: usize = 1024;
 
+#[derive(Debug)]
 enum Error {
     StackOverflow,
     StackUnderflow,
     SegmentationFault,
     IllegalInst,
+    DivByZero,
+    IllegalJmp,
 }
 
+#[derive(Debug, Clone)]
 enum InstType {
     Push,
     Plus,
     Sub,
     Mul,
     Div,
+    Jmp,
 }
 
+#[derive(Debug, Clone)]
 struct Inst {
     inst_type: InstType,
-    operand: Word
+    operand: Word,
 }
 
 impl Inst {
@@ -29,46 +35,107 @@ impl Inst {
 
 struct Machine {
     stack: [Word; STACK_CAP],
-    stack_size: usize,
+    sp: usize,
+    ip: usize,
+    program: Vec<Inst>
 }
 
 impl Machine {
-    fn new() -> Self {
-        Machine { stack: [0; STACK_CAP], stack_size: 0 }
+    fn new(program: Vec<Inst>) -> Self {
+        Machine {
+            stack: [0; STACK_CAP],
+            sp: 0,
+            ip: 0,
+            program
+        }
     }
 
-    fn exec(&mut self, inst: &Inst) -> Result<(), Error> {
-        
+    /// Increments the ip and executes next instruction
+    fn exec(&mut self) -> Result<(), Error> {
+        while self.ip < self.program.len() {
+            let inst = self.program[self.ip].clone();
+            self.ip += 1;
+            self.exec_inst(&inst)?;
+        }
+        Ok(())
+    }
+
+    fn exec_inst(&mut self, inst: &Inst) -> Result<(), Error> {
         match inst.inst_type {
             InstType::Push => {
-                self.stack[self.stack_size] = inst.operand;
-                self.stack_size += 1;
-            },
-            InstType::Plus => {
-            },
-            InstType::Sub => {
-            },
-            InstType::Mul => {
-            },
-            InstType::Div => {
+                if self.sp >= STACK_CAP {
+                    return Err(Error::StackOverflow);
+                }
+                self.stack[self.sp] = inst.operand;
+                self.sp += 1;
             }
-            _ => return Err(Error::IllegalInst)
-        }
+            InstType::Plus => {
+                if self.sp < 2 {
+                    return Err(Error::StackUnderflow);
+                }
+                self.stack[self.sp - 2] += self.stack[self.sp - 1];
+                self.sp -= 1;
+            }
+            InstType::Sub => {
+                if self.sp < 2 {
+                    return Err(Error::StackUnderflow);
+                }
+                self.stack[self.sp - 2] -= self.stack[self.sp - 1];
+                self.sp -= 1;
+            }
+            InstType::Mul => {
+                if self.sp < 2 {
+                    return Err(Error::StackUnderflow);
+                }
+                self.stack[self.sp - 2] *= self.stack[self.sp - 1];
+                self.sp -= 1;
+            }
+            InstType::Div => {
+                if self.sp < 2 {
+                    return Err(Error::StackUnderflow);
+                }
+                else if self.stack[self.sp - 2] == 0 || self.stack[self.sp - 1] == 0 {
+                    return Err(Error::DivByZero);
+                }
 
+                self.stack[self.sp - 2] /= self.stack[self.sp - 1];
+                self.sp -= 1;
+            }
+            InstType::Jmp => {
+                if inst.operand as usize > self.program.len() || inst.operand < 0 {
+                    return Err(Error::IllegalJmp);
+                }
+                
+                self.ip = inst.operand as usize;
+            }
+        }
         Ok(())
     }
 
     fn dump(&self) {
         println!("Stack:");
-        for i in 0..self.stack_size {
-            println!("  {}", self.stack[i]);
+        for i in 0..self.sp {
+            println!("  {} - {}", self.stack[i], i);
         }
     }
 }
 
-fn main() {
-    let mut machine = Machine::new();
-    let inst = Inst::new(InstType::Push, 69);
-    let _ = machine.exec(&inst);
+// TODO: Load Program from file (.cvm)
+
+fn main() -> Result<(), Error> {
+
+    let program = vec![
+        Inst::new(InstType::Push, 69),
+        Inst::new(InstType::Push, 2),
+        Inst::new(InstType::Mul, 0),
+        Inst::new(InstType::Push, 2),
+        Inst::new(InstType::Div, 0),
+    ];
+
+    let mut machine = Machine::new(program);
+    let _ = machine.exec()?;
     let _ = machine.dump();
+
+    Ok(())
 }
+
